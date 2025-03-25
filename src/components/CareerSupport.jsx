9c844eb9db3518b7834983a5fdbb6d6b5c4c1a33
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-03-25 12:19:23 +0530

    init

--- src/components/CareerSupport.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CareerSupport() {
-  return (
-    <div>CareerSupport</div>
-  )
-}
-
-export default CareerSupport
--- src/components/Enquiry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Enquiry() {
-  return (
-    <div>Enquiry</div> // This is a placeholder for the Enquiry component
-  )
-}
-
-export default Enquiry
