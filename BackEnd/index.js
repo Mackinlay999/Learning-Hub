7e419b33c1d2d34e73cd37ad724bcd93fcc4adf9
Jerald321 <rjerald6803@gmail.com>
2025-03-25 10:26:17 +0530

    start-1

---
+++ BackEnd/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const app =express() 
+
+
+
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+module.exports = app;
