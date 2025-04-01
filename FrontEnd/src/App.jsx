ffb0e4c7868f0d6fed30599c7a20dc57bf86baaa
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-04-01 19:04:22 +0530

    init

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/career-support" element={<Career />} />
-          <Route path="/success-story" element={<SuccessStory />} />
+          {/* <Route path="/success-story" element={<SuccessStory />} /> */}
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blog-training" element={<BlogTraining />} />
