526fe161cc2b534b46083ad6e1b9b7b7f150d38c
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-04-04 17:50:26 +0530

    change

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true }) // opens bundle report after build
+    // visualizer({ open: true }) // opens bundle report after build
   ],
   build: {
     chunkSizeWarningLimit: 1000,
