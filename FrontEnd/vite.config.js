d3f2b664e48396929edab42fb8d821f5c4c794ae
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-04-04 18:05:37 +0530

    init

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { visualizer } from 'rollup-plugin-visualizer'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // visualizer({ open: true }) // opens bundle report after build
-  ],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['axios'],
-        },
-      },
-    },
-  },
+  plugins: [react()],
 })
