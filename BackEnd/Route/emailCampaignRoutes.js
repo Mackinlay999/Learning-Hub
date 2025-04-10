f860f8c962efe8855e6ab2ff12b4703d75026552
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-04-10 18:17:52 +0530

    ref

--- BackEnd/Route/emailCampaignRoutes.js
+++ BackEnd/Route/emailCampaignRoutes.js
@@ -7,7 +7,6 @@ router.get("/email-campaigns", emailCampaignController.getAllCampaigns);
 router.post("/email-campaigns", emailCampaignController.createCampaign);
 router.put("/email-campaigns/:id", emailCampaignController.updateCampaign);
 router.delete("/email-campaigns/:id", emailCampaignController.deleteCampaign);
-
 router.post("/email-campaigns/:id/emails", emailCampaignController.addEmailToCampaign);
 router.delete("/email-campaigns/:campaignId/emails/:emailId", emailCampaignController.deleteEmailFromCampaign);
 
