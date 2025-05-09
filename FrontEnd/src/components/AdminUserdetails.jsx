4e3b7ccd06f636736abdaea8b3ff07b1180c549c
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-05-09 18:45:31 +0530

    Done

--- FrontEnd/src/components/AdminUserdetails.jsx
+++ FrontEnd/src/components/AdminUserdetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from './axios';
-import '../style/AdminUserdetails.css';
+import '../style/AdminUserDetails.css';
 
 const AdminUserdetails = () => {
   const [name, setName] = useState('');
