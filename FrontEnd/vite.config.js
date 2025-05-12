0c2c419d460ef8846401b376f69624f8a1830867
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-05-12 15:46:08 +0530

    Solved Netlify Issue

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react';
+            if (id.includes('axios')) return 'axios';
+            if (id.includes('lodash')) return 'lodash';
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 900, // Optional: raise warning limit (default is 500)
+  },
 })
