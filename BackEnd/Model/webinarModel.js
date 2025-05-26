498804e8dc1f071dad9bfa7219a9386c49a1f2cb
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-05-27 01:04:17 +0530

    err

--- BackEnd/Model/webinarModel.js
+++ BackEnd/Model/webinarModel.js
@@ -1,19 +1,11 @@
 const mongoose = require("mongoose");
 
-const webinarSchema = new mongoose.Schema(
-  {
-    // name: { type: String, required: true },
-    // email: { type: String, required: true, unique: true },
-    // phone: { type: String },
-    webinarTitle: { type: String, required: true },
-    webinarDateTime: { type: Date, required: true },
-    webinarDescription: { type: String, required: true },
-    webinarLink: { type: String,required: true  }, // New field for webinar link
-    typeofProgram: { type: String,required: true  }, 
-    // registrants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    createdAt: { type: Date, default: Date.now },
-  },
-  { timestamps: true }
-);
+const webinarSchema = new mongoose.Schema({
+  webinarTitle: { type: String, required: true },
+  webinarDateTime: { type: Date, required: true },
+  webinarDescription: { type: String, required: true },
+  webinarLink: { type: String, required: true },
+  typeofProgram: { type: String, required: true },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Webinar", webinarSchema);
